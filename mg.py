32e2d4cec994dd424942e62c674401afd2c3b15b
Smirnova-Hope <96446588+Smirnova-Hope@users.noreply.github.com>
2022-04-18 19:16:39 +0300

    Update mg.py

--- mg.py
+++ mg.py
@@ -1,86 +1,69 @@
-from random import choice
+import telebot
+from jjj import get_map_cell
+
+bot = telebot.TeleBot('zzzzzzzzzzzzz')
+cols, rows = 8, 8
+maps = {}
+
+keyboard = telebot.types.InlineKeyboardMarkup()
+keyboard.row( telebot.types.InlineKeyboardButton('←', callback_data='left'),
+              telebot.types.InlineKeyboardButton('↑', callback_data='up'),
+              telebot.types.InlineKeyboardButton('↓', callback_data='down'),
+              telebot.types.InlineKeyboardButton('→', callback_data='right'))
+
+def get_map_str(map_cell, player):
+    map_str = ''
+    for y in range(rows * 2 - 1):
+        for x in range(cols * 2 - 1):
+            if map_cell[x + y * (cols * 2 - 1)]:
+                map_str += "⬛"
+            elif (x, y) == player:
+                map_str += "🔴"
+            else:
+                map_str += "⬜"
+        map_str += '\n'
+    return map_str
 
 
-def get_map_cell(cols, rows):
-	class Cell:
-		def __init__(self, x, y):
-			self.x = x
-			self.y = y
-			self.walls = {'top': True, 'right': True, 'bottom': True, 'left': True}
-			self.visited = False
+@bot.message_handler(commands=['play'])
+def play(message):
+    map_cell = get_map_cell(cols, rows)
+    user_data = {
+        'map': map_cell,
+        'x': 0,
+        'y': 0
+    }
+    maps[message.chat.id] = user_data
+    bot.send_message(message.from_user.id, get_map_str(map_cell, (0, 0)), reply_markup=keyboard)
 
-		def check_cell(self, x, y):
-			if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
-				return False
-			return grid_cell[x + y * cols]
+@bot.callback_query_handler(func=lambda call: True)
+def callback_func(query):
+    user_data = maps[query.message.chat.id]
+    n_x, n_y = user_data['x'], user_data['y']
+    if query.data == 'left':
+        n_x -= 1
+    if query.data == 'right':
+        n_x += 1
+    if query.data == 'up':
+        n_y -= 1
+    if query.data == 'down':
+        n_y += 1
 
-		def check_neighbours(self):
-			neighbours = []
+    if n_x < 0 or n_x > 2 * cols - 2 or n_y < 0 or n_y > rows * 2 - 2:
+        return None
+    if user_data['map'][n_x + n_y * (cols * 2 - 1)]:
+        return None
 
-			top = self.check_cell(self.x, self.y - 1)
-			right = self.check_cell(self.x + 1, self.y)
-			bottom = self.check_cell(self.x, self.y + 1)
-			left = self.check_cell(self.x - 1, self.y)
+    if n_x == cols * 2 - 2 and n_y == rows * 2 - 2:
+        bot.edit_message_text(chat_id=query.message.chat.id,
+                              message_id=query.chat.id,
+                              text="Вы выиграли",
+                              reply_markup=keyboard)
+        return None
+    user_data['x'], user_data['y'] = n_x, n_y
+    bot.edit_message_text(chat_id=query.message.chat.id,
+                          message_id=query.chat.id,
+                          text=get_map_str(user_data['map'], (n_x, n_y)),
+                          reply_markup=keyboard)
 
-			if top and not top.visited:
-				neighbours.append(top)
-			if right and not right.visited:
-				neighbours.append(right)
-			if bottom and not bottom.visited:
-				neighbours.append(bottom)
-			if left and not left.visited:
-				neighbours.append(left)
-
-			return choice(neighbours) if neighbours else False
-
-
-	def remove_walls(current_cell, next_cell):
-		dx = current_cell.x - next_cell.x
-		dy = current_cell.y - next_cell.y
-
-		if dx == 1:
-			current_cell.walls['left'] = False
-			next_cell.walls['right'] = False
-		if dx == -1:
-			current_cell.walls['right'] = False
-			next_cell.walls['left'] = False
-		if dy == 1:
-			current_cell.walls['top'] = False
-			next_cell.walls['bottom'] = False
-		if dy == -1:
-			current_cell.walls['bottom'] = False
-			next_cell.walls['top'] = False
-
-	def check_wall(grid_cell, x, y):
-		if x % 2 == 0 and y % 2 == 0:
-			return False
-		if x % 2 == 1 and y % 2 == 1:
-			return True
-
-		if x % 2 == 0:
-			grid_x = x // 2
-			grid_y = (y - 1) // 2
-			return grid_cell[grid_x + grid_y * cols].walls['bottom']
-		else:
-			grid_x = (x - 1) // 2
-			grid_y = y // 2
-			return grid_cell[grid_x + grid_y * cols].walls['right']
-
-	grid_cell = [Cell(x, y) for y in range(rows) for x in range(cols)]
-	current_cell = grid_cell[0]
-	current_cell.visited = True
-	stack = []
-
-	while True:
-		next_cell = current_cell.check_neighbours()
-		if next_cell:
-			next_cell.visited = True
-			remove_walls(current_cell, next_cell)
-			current_cell = next_cell
-			stack.append(current_cell)
-		elif stack:
-			current_cell = stack.pop()
-		else:
-			break
-
-	return [check_wall(grid_cell, x, y) for y in range(rows * 2 - 1) for x in range(cols * 2 - 1)]
+bot.polling(none_stop=False, interval=0)
